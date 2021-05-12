7c79962f2341c1fca394a4de235aa0b54d8bb9ad
LauriTahvanainen <48516187+LauriTahvanainen@users.noreply.github.com>
2021-05-12 11:00:37 +0200

    Connect Metamask account on load if not connected

--- src/App.js
+++ src/App.js
@@ -8,12 +8,48 @@ import Navbar from './components/Layout/Navbar';
 import Homepage from './pages/Homepage';
 import Event from './pages/Event';
 import CreateBet from './pages/CreateBet';
-import PlaceBet from './pages/PlaceBet';
 import GambleBoard from './abis/GambleBoard.json'
+import Web3 from 'web3'
 
-const contractAdress = "0x6aa92B1D78e7bEfBEE724dE3AA35CD34BB52c606";
+const contractAdress = "0x2E6318CC9006f132265fDBf9F68569172101Bb6f";
 
 function App() {
+
+  const web3 = new Web3(window.ethereum);
+
+  const contract = new web3.eth.Contract(
+    GambleBoard,
+    contractAdress
+  );
+
+  const [account, setAccount] = useState(0);
+
+  useEffect(() => {
+    getAccount();
+  }, [account]);
+
+  const getAccount = useCallback(async () => {
+    if (window.ethereum) {
+      try {
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        setAccount(accounts[0]);
+      } catch (error) {
+        if (error.code === 4001) {
+          // User rejected request
+          alert("Connect account to use the application!")
+        } else {
+          alert("Error connecting Metamask")
+        }
+      }
+    } else {
+      alert("Please install metamask!")
+    }
+    window.ethereum.on("accountsChanged", function (accounts) {
+      setAccount(accounts[0]);
+      console.log(accounts[0])
+    });
+  }, [account]);
+
 
   return (
     <div className="d-flex flex-column" style={{ height: '100vh' }}>
