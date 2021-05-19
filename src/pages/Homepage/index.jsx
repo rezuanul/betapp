41d77ae208df1e702b0ab7019248ae943bdab428
Ronin995 <46960284+Ronin995@users.noreply.github.com>
2021-05-19 12:30:03 +0200

    Overflow resolution
    incapsuled the main row in a column

--- src/pages/Homepage/index.jsx
+++ src/pages/Homepage/index.jsx
@@ -75,97 +75,98 @@ export default function Homepage({ account, filters, setFilters, loading, error,
     <>
       <PageCover description={"Search upcoming events"} />
       <div className="homepage">
-        <div className="mt-5 row">
+        <div className="col">
+          <div className="mt-5 row">
 
-          <div className="col-sm-2">
-            <div className="container">
+            <div className="col-sm-2">
+              <div className="container">
 
-              <div className="btn btn-block mt-3">
-                <Button href="/create-bet" variant="success" block>
-                  Create a Bet
-                    </Button>
+                <div className="btn btn-block mt-3">
+                  <Button href="/create-bet" variant="success" block>
+                    Create a Bet
+                      </Button>
+                </div>
+
+                <div className="btn btn-block mt-3">
+                  <Button variant="warning" onClick={resetFilters} block>
+                    Show all events
+                   </Button>
+                </div>
+
+                <div className="btn btn-block mt-3">
+                  <Button variant="outline-primary" onClick={reloadHandler} block>
+                    Reload with current filters
+                   </Button>
+                </div>
+
               </div>
+            </div>
 
-              <div className="btn btn-block mt-3">
-                <Button variant="warning" onClick={resetFilters} block>
-                  Show all events
-                 </Button>
-              </div>
+            <div className="col mt-3">
 
-              <div className="btn btn-block mt-3">
-                <Button variant="outline-primary" onClick={reloadHandler} block>
-                  Reload with current filters
-                 </Button>
+              <div className="row">
+
+                <div className="col">
+                  <Form>
+                    <Form.Group controlId="SelectCountry">
+                      <Form.Label>Country</Form.Label>
+                      <Form.Control
+                        value={((filters.country) ? filters.country : 0)}
+                        onChange={countryFilterHandler}
+                        as="select"
+                        custom
+                      >
+                        {countryOptionsArray}
+                      </Form.Control>
+                    </Form.Group>
+                  </Form>
+                </div>
+
+                <div className="col">
+                  <Form>
+                    <Form.Group controlId="SelectCategory">
+                      <Form.Label>Category</Form.Label>
+                      <Form.Control
+                        value={((filters.category) ? filters.category : 0)}
+                        onChange={categoryFilterHandler}
+                        as="select"
+                        custom
+                      >
+                        {categoryOptionsArray}
+                      </Form.Control>
+                    </Form.Group>
+                  </Form>
+                </div>
+
+                <div className="col">
+                  <Form>
+                    <Form.Group controlId="SelectLeague">
+                      <Form.Label>League</Form.Label>
+                      <Form.Control
+                        className="w-100"
+                        value={filters.league}
+                        onChange={leagueFilterHandler}
+                        as="input"
+                        custom
+                      >
+
+                      </Form.Control>
+                    </Form.Group>
+                  </Form>
+                </div>
+
               </div>
+              <EventTable
+                betData={data && { data }}
+                error={error}
+                loading={loading}
+                showBetsHandler={showBetsHandler}
+              />
 
             </div>
-          </div>
-
-          <div className="col mt-3">
-
-            <div className="row">
-
-              <div className="col">
-                <Form>
-                  <Form.Group controlId="SelectCountry">
-                    <Form.Label>Country</Form.Label>
-                    <Form.Control
-                      value={((filters.country) ? filters.country : 0)}
-                      onChange={countryFilterHandler}
-                      as="select"
-                      custom
-                    >
-                      {countryOptionsArray}
-                    </Form.Control>
-                  </Form.Group>
-                </Form>
-              </div>
-
-              <div className="col">
-                <Form>
-                  <Form.Group controlId="SelectCategory">
-                    <Form.Label>Category</Form.Label>
-                    <Form.Control
-                      value={((filters.category) ? filters.category : 0)}
-                      onChange={categoryFilterHandler}
-                      as="select"
-                      custom
-                    >
-                      {categoryOptionsArray}
-                    </Form.Control>
-                  </Form.Group>
-                </Form>
-              </div>
-
-              <div className="col">
-                <Form>
-                  <Form.Group controlId="SelectLeague">
-                    <Form.Label>League</Form.Label>
-                    <Form.Control
-                      className="w-100"
-                      value={filters.league}
-                      onChange={leagueFilterHandler}
-                      as="input"
-                      custom
-                    >
-
-                    </Form.Control>
-                  </Form.Group>
-                </Form>
-              </div>
-
-            </div>
-            <EventTable
-              betData={data && { data }}
-              error={error}
-              loading={loading}
-              showBetsHandler={showBetsHandler}
-            />
 
           </div>
-
         </div>
-
       </div>
     </>
   );
